19003894947076beeca0a67509a66b61b3f138b5
Priyanon16 <66010912608@msu.ac.th>
2026-02-14 13:53:12 +0700

    a

--- back/login.php
+++ back/login.php
@@ -1,46 +1,59 @@
 <?php
-    session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="th">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ปรียานนท์ กรุตนิด(มินนี่)</title>
+<meta charset="UTF-8">
+<title>SNEAKERHUB - Login</title>
 </head>
 <body>
-    
-<h1>เข้าสู่ระบบหลังบ้าน - ปรียานนท์</h1>
+
+<h2>เข้าสู่ระบบ SNEAKERHUB</h2>
+
 <form method="post" action="">
-    Username <input type="text" name="auser" autofocus required><br>
-    Password <input type="password" name="apwd" required><br>
+    Email :
+    <input type="email" name="email" required autofocus><br><br>
+
+    Password :
+    <input type="password" name="password" required><br><br>
+
     <button type="submit" name="Submit">LOGIN</button>
 </form>
 
 <?php
 if(isset($_POST['Submit'])) {
-    include_once("connectdb.php");
-    $sql = "SELECT * FROM admin WHERE a_username='{$_POST['auser']}' AND a_password='{$_POST['apwd']}' LIMIT 1 ";
+
+    include_once("connectdb.php");  // ไฟล์เชื่อมต่อฐานข้อมูล
+
+    $sql = "SELECT * FROM users 
+            WHERE email='{$_POST['email']}' 
+            AND password='{$_POST['password']}' 
+            LIMIT 1";
+
     $rs = mysqli_query($conn,$sql);
     $num = mysqli_num_rows($rs);
 
     if($num == 1) {
+
         $data = mysqli_fetch_array($rs);
-        $_SESSION['aid'] = $data['a_id'];
-        $_SESSION['aname'] = $data['a_name'];
-        echo"<script>" ;
+
+        $_SESSION['uid'] = $data['id'];
+        $_SESSION['uname'] = $data['name'];
+
+        echo "<script>";
         echo "window.location='index.php';";
         echo "</script>";
+
     } else {
-        echo"<script>" ;
-        echo "alert('Username หรือ Password ไม่ถูกต้อง');";
+
+        echo "<script>";
+        echo "alert('Email หรือ Password ไม่ถูกต้อง');";
         echo "</script>";
     }
-
 }
 ?>
-
 
 </body>
 </html>
