4267a9fc9b037c4ce28e0c0bbcb47e3ce9a9ecb0
Priyanon16 <66010912608@msu.ac.th>
2026-02-15 21:41:32 +0700

    a

--- sidebar.php
+++ sidebar.php
@@ -8,13 +8,6 @@ session_start();
 <title>Sidebar</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- Bootstrap 5 -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Bootstrap Icons -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-
 
 <style>
 body {
@@ -221,8 +214,6 @@ body {
     </div>
 </div>
 
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 <script>
 document.getElementById('toggleBtn').addEventListener('click', function () {
