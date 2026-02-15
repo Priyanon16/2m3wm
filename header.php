9e449eeae60654b70ba25e1adb80eef5a3da7b86
Priyanon16 <66010912608@msu.ac.th>
2026-02-15 13:13:27 +0700

    a

--- header.php
+++ header.php
@@ -178,9 +178,9 @@ $totalFav  = count($_SESSION['favorite']);
                     <i class="bi bi-clock-history me-2 text-success"></i>ประวัติการสั่งซื้อ
                 </a>
             </li>
-             <li>
-                <a class="dropdown-item" href="address.php">
-                    <i class="bi bi-box-history me-2 text-danger"></i>ที่อยู่จัดส่ง
+            <li>
+                <a class="dropdown-item" href="customer_address.php">
+                    <i class="bi bi-geo-alt me-2 text-danger"></i>ที่อยู่จัดส่ง
                 </a>
             </li>
         </ul>
