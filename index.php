1d9f90b368c84f772bb79b422aad4e4350cad051
Priyanon16 <66010912608@msu.ac.th>
2026-02-26 22:12:39 +0700

    a

--- index.php
+++ index.php
@@ -233,12 +233,6 @@ $discount = $row['discount_percent'] ?? 0;
 $is_promo = $row['is_promo'] ?? 0;
 ?>
 
-<?php if($is_promo == 1 && $discount > 0): ?>
-<div class="promo-badge">
-    ลด <?= $discount ?>%
-</div>
-<?php endif; ?>
-
 <a href="product_detail.php?id=<?= $row['p_id']; ?>" 
    class="text-decoration-none text-dark">
 
